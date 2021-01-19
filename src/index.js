375ab175d4670730d18adca34284a9b25feefdeb
gracenak <gracenak@gmail.com>
2021-01-19 13:11:18 -0700

    commit tree b91391e72c3d8fc5d3df0368feb3b3c08f61c6eb

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function fetchDogs() {
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(data => {
+
+    data.message.forEach(dogImage => {
+        const imageContainer = document.querySelector('#dog-image-container')
+        const img = document.createElement('img')
+        img.src = dogImage
+        imageContainer.appendChild(img)
+    })
+})
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchDogs()
+})
